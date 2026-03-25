2339675006f0c9cface2e8df3257a3dffc2ac73e
Bark Maxim <maximalen1999@gmail.com>
2026-03-25 05:15:21 +0300

    Factory Pattern added

---
+++ src/main/java/factory/People.java
@@ -0,0 +1,8 @@
+package factory;
+
+import java.util.UUID;
+
+public interface People {
+    public UUID getId();
+    public String getName();
+}
---
+++ src/main/java/factory/PeopleFactory.java
@@ -0,0 +1,17 @@
+package factory;
+
+import com.example.demo.entity.Director;
+import com.example.demo.entity.Employee;
+
+public class PeopleFactory {
+    public static People createPeople(PeopleType peopleType) {
+        switch (peopleType) {
+            case Director:
+                return new Director();
+            case Employee:
+                return  new Employee();
+            default:
+                return null;
+        }
+    }
+}
